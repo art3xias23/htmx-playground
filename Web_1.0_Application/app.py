aae368d49ae83a8c2d4767c790144b08e3ba2078
Tino <Konstantin.v.milchev@gmail.com>
2023-11-03 16:01:48 +0000

    Added base demo app with form and url

--- Web_1.0_Application/app.py
+++ Web_1.0_Application/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import (Flask, redirect, render_template, request)
+from contacts_model import (Contact)
+
+Contact.load_db()
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
     return redirect('/contacts')
+
+@app.route('/contacts')
+def contacts():
+    search = request.args.get("q")
+    if search is not None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all()
+    return render_template("index.html", contacts=contacts_set)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
